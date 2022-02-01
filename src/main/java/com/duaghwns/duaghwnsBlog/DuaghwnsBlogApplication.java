660d9e61bc8b58da928c109e54d4b89909025c41
duaghwns <hj2758@gmail.com>
2022-02-01 16:44:23 +0900

    프로젝트 세팅

---
+++ src/main/java/com/duaghwns/duaghwnsBlog/DuaghwnsBlogApplication.java
@@ -0,0 +1,13 @@
+package com.duaghwns.duaghwnsBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuaghwnsBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuaghwnsBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duaghwns/duaghwnsBlog/DuaghwnsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duaghwns.duaghwnsBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuaghwnsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
