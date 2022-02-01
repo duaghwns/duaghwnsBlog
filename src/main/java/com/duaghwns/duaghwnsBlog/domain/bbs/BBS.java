34064c23e86cf27027c32279b8d0e2e349ffecdc
duaghwns <hj2758@gmail.com>
2022-02-01 17:33:41 +0900

    엔티티 매핑

--- src/main/java/com/duaghwns/duaghwnsBlog/domain/bbs/BBS.java
+++ src/main/java/com/duaghwns/duaghwnsBlog/domain/bbs/BBS.java
@@ -1,4 +1,26 @@
 package com.duaghwns.duaghwnsBlog.domain.bbs;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class BBS {
+    @Id
+    private int bbs_seq;
+    @Column(name = "TITLE", length = 30)
+    private String title;
+
+    @Column(name = "CONTENT")
+    @Lob
+    private String content;
+    @Column(name = "AUTHOR", length = 15)
+    private String author;
+    @Column(name = "REG_DTE")
+    @Temporal(TemporalType.DATE)
+    private Date regdate;
+    @Column(name = "UPT_DTE")
+    @Temporal(TemporalType.DATE)
+    private Date moddate;
+
+
 }
--- src/main/java/com/duaghwns/duaghwnsBlog/domain/user/User.java
+++ src/main/java/com/duaghwns/duaghwnsBlog/domain/user/User.java
@@ -1,4 +1,26 @@
 package com.duaghwns.duaghwnsBlog.domain.user;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id @Column(name = "USR_SEQ")
+    private int user_seq;
+
+    @Column(name = "ID",length = 15)
+    private String userid;
+
+    @Column(name = "NM",length = 15)
+    private String usernm;
+
+    @Column(name = "PW",length = 20)
+    private String userpw;
+
+    @Column(name = "NICK",length = 10)
+    private String usernick;
+
+    @Column(name = "BIRTH")
+    @Temporal(TemporalType.DATE)
+    private Date birth;
 }
